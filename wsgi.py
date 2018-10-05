dc56cbbe20626220589f1a032c05526840bfc750
richardo1459 <30681998+richardo1459@users.noreply.github.com>
2018-10-05 15:56:51 +0700

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def hello():
+    return "OpenShift Hello World!"
+    return "Created by Richardo"
+    return "Friday, October 5th, 2018"
+
+if __name__ == "__main__":
+    application.run()
