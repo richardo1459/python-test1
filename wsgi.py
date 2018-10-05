315854c8b7d6ac11687f16ab4ee28355774d2f0f
richardo1459 <richardo1459@gmail.com>
2018-10-05 16:17:43 +0700

    Change greeting.

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,9 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "OpenShift Hello World!"
+    #return "OpenShift Hello World!"
     return "Created by Richardo"
-    return "Friday, October 5th, 2018"
+    #return "Friday, October 5th, 2018"
 
 if __name__ == "__main__":
     application.run()
